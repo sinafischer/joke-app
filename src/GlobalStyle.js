3a3c88364d0c4791ab0eac360f26a6332c0a5869
sinafischer <sina.h.fischer@gmail.com>
2020-09-24 23:21:01 +0200

    Initial Commit

---
+++ src/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+* {
+  box-sizing: border-box;
+}
+  
+body {
+  font-size: 18px;
+  font-family: sans-serif;
+  height: 100vh;
+  margin: 0;
+}
+
+`;
---
+++ src/pages/JokesPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function JokesPage() {
+  return (
+    <div>
+      <p>Who is the coolest Doctor in the hospital?</p>
+      <p>The hip doctor</p>
+    </div>
+  )
+}
