66009df9ee0d496208c15264bd0abfe1359e9e51
sinafischer <sina.h.fischer@gmail.com>
2020-10-04 11:39:01 +0200

    Implement API fetch
    ..

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react'
-import JokesPage from './pages/JokesPage'
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  return <JokesPage />
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    fetch('https://official-joke-api.appspot.com/random_ten')
+      .then((response) => response.json())
+      .then((data) => {
+        setJokes(data)
+      })
+      .catch((error) => {
+        console.log(error.message)
+      })
+  }, [])
+
+  console.log(jokes)
+
+  return (
+    <>
+      {jokes &&
+        jokes.map((joke) => {
+          return (
+            <ul key={joke.id}>
+              <li>
+                <p>{joke.setup}</p>
+                <p>{joke.punchline}</p>
+              </li>
+            </ul>
+          )
+        })}
+    </>
+  )
 }
 
 export default App
